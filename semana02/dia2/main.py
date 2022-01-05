93bd9e67a651600101aaec2aed6653802b098c53
cesar mayta <cesarmayta@gmail.com>
2022-01-04 22:39:12 -0500

    portafolio github

--- semana02/dia2/main.py
+++ semana02/dia2/main.py
@@ -1,10 +1,18 @@
 from flask import Flask,render_template,request
+import requests
+
+URL = 'https://api.github.com/users/cesarmayta'
+
+#CAPTURAMOS LOS DATOS DE MI CUENTA DE GITHUB
+data = requests.get(URL)
+context = data.json()
+print(context)
 
 app = Flask(__name__)
 
 lstProductos = ['LAPTOP','IMPRESORA','PARLANTES']
 
-@app.route('/')
+@app.route('/index')
 def index():
     #return 'HOLA MUNDO FLASK'
     nombre = request.args.get('nombre','no hay nombre')
@@ -16,11 +24,21 @@ def index():
     
     return render_template('index.html',**context)
 
+@app.route('/')
+def home():
+    return render_template('home.html',**context)
+
 @app.route('/portafolio')
 def portafolio():
     return render_template('portafolio.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
