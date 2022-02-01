7f3c1b2a9202d254666053e253683aa4d8d4dec7
cesar mayta <cesarmayta@gmail.com>
2022-01-31 20:41:45 -0500

    dia4

---
+++ semana05/dia4/pos_backend/api/views.py
@@ -0,0 +1,56 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+from .models import *
+from .serializers import *
+
+class IndexView(APIView):
+    
+    def get(self,request):
+        context = {'ok':True,
+                   'message':'el servidor está activo!'
+                   }
+        return Response(context)
+    
+class CategoriaView(APIView):
+    
+    def get(self,request):
+        dataCategoria = Categoria.objects.all()
+        serCategoria = CategoriaSerializer(dataCategoria,many=True)
+        return Response({'ok':True,
+                        'content':serCategoria.data})
+        
+class MesaView(APIView):
+    
+    def get(self,request):
+        dataMesa = Mesa.objects.all()
+        serMesa = MesaSerializer(dataMesa,many=True)
+        context = {
+            'ok':True,
+            'content': serMesa.data
+        }
+        return Response(context)
+    
+class CategoriaPlatosView(APIView):
+    
+    def get(self,request,categoria_id):
+        dataCategoria = Categoria.objects.get(pk=categoria_id)
+        serCategoria = CategoriaPlatosSerializer(dataCategoria)
+        context = {
+            'ok':True,
+            'content':serCategoria.data
+        }
+        return Response(context)
+    
+class PedidoView(APIView):
+    
+    def post(self,request):
+        serPedido  = PedidoSerializerPOST(data=request.data)
+        serPedido.is_valid(raise_exception=True)
+        serPedido.save()
+        
+        return Response({
+            'ok':True,
+            'content':serPedido.data
+        })
---
+++ semana05/dia4/pos_frontend/src/environments/environments.js
@@ -0,0 +1,6 @@
+export const URL_BACKEND =
+	'http://127.0.0.1:8000'	
+	//'https://backposapi.herokuapp.com'
+	//'http://localhost:8000';
+	//'https://punto-de-venta-codigo-backend.herokuapp.com'
+	
