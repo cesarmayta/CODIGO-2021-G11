d786518e823aee155f4d590856d88683e3de03b1
cesar mayta <cesarmayta@gmail.com>
2022-02-10 22:31:47 -0500

    dia4 final

---
+++ semana07/dia4/pos_frontend/src/environments/environments.js
@@ -0,0 +1,8 @@
+export const URL_BACKEND =
+	'http://52.142.45.128'
+	//'https://cmaytabackendpos.herokuapp.com'
+	//'http://127.0.0.1:8000'	
+	//'https://backposapi.herokuapp.com'
+	//'http://localhost:8000';
+	//'https://punto-de-venta-codigo-backend.herokuapp.com'
+	
