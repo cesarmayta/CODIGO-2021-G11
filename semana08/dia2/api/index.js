99a7c2ec75095d0543dcc918cbea218076b5aa2a
cesar mayta <cesarmayta@gmail.com>
2022-02-17 18:56:44 -0500

    api 2

--- semana08/dia2/api/index.js
+++ semana08/dia2/api/index.js
@@ -27,6 +27,58 @@ app.get('/employee',function(req,res){
     })
 })
 
+app.post('/employee',function(req,res){
+    const {name,salary} = req.body;
+    const query = 'insert into employee(name,salary) values(?,?)';
+    
+    mysqlConnection.query(query,[name,salary],(err,rows,fields)=>{
+        if(!err){
+            res.json({
+                'status':true,
+                'content':'employed inserted'
+            })
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
+
+app.put('/employee/:id',function(req,res){
+    const {name,salary} = req.body;
+    const {id} = req.params;
+    const query = "update employee set name=?,salary=? where id=?"
+
+    mysqlConnection.query(query,[name,salary,id],(err,rows,fields)=>{
+        if(!err){
+            res.json({
+                'status':true,
+                'content':'Employee Updated'
+            })
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+app.delete('/employee/:id',function(req,res){
+    const {id} = req.params;
+    const query = "delete from employee where id=?"
+
+    mysqlConnection.query(query,[id],(err,rows,fields)=>{
+        if(!err){
+            res.json({
+                'status':true,
+                'content':'Employee Deleted'
+            })
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+
+
 //Server
 app.listen(app.get('port'),() =>{
     console.log(`Server running at http://localhost:${app.get('port')}`);
