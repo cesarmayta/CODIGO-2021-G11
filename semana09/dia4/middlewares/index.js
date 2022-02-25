d2f840ebe00723a406cb3187aedeac349c654127
cesar mayta <cesarmayta@gmail.com>
2022-02-24 20:06:02 -0500

    dia4

---
+++ semana09/dia4/middlewares/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const boom = require('@hapi/boom');
+
+//MIDDLEWARES PRINCIPALES
+app.use(function(req,res,next){
+    console.log('Tiempo : ',Date.now());
+    next();
+})
+
+//rutas
+app.get("/",(req,res)=>{
+    res.json({
+        status:true,
+        content:"EJEMPLOS CON MIDDLEWARES"
+    })
+})
+
+//middlware para ruta usuario
+app.use('/usuario',function(req,res,next){
+    console.log(a + 3);
+    console.log('tipo request : ',req.method);
+    next();
+})
+
+app.get("/usuario",(req,res)=>{
+    res.json({
+        status:true,
+        content:[{
+            nombre:'cmayta',
+            email:'cesarmayta@gmail.com'
+        }]
+    })
+})
+
+//MIDDLEWARE PARA ERRORES
+app.use(function(err,req,res,next){
+    console.error(err.stack);
+    res.status(500).json({
+        status:false,
+        content:'ocurrio un error en el servidor'
+    })
+})
+
+app.listen(5000,function(){
+    console.log('servidor corriendo en http://localhost:5000');
+})
